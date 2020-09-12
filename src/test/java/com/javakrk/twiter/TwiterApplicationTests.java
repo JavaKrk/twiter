6b9d980696b3bf65ba03f6b33f61199d60363ebe
Tarriamr <pawel.putala@gmail.com>
2020-09-12 13:23:41 +0200

    Initial commit

---
+++ src/test/java/com/javakrk/twiter/TwiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javakrk.twiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
