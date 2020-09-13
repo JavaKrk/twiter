6fe78437f873f368bc7ff9b989ae1a126b1d7cb4
SzymonAfek <szymon1106@gmail.com>
2020-09-13 14:25:54 +0200

    Main Page new feature

--- src/main/java/com/javakrk/twiter/controller/UserController.java
+++ src/main/java/com/javakrk/twiter/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class UserController {
 
-    @GetMapping("/login")
+    @GetMapping("/start")
     public String getLoginView() {
         return "login";
     }
